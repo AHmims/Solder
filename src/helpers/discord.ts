f1dededc7d4dfcec4354dce2e18363eb781c9e8f
Ali HMIMS <ali.hmims99@gmail.com>
2021-11-10 19:31:37 +0100

    huh?

--- src/helpers/discord.ts
+++ src/helpers/discord.ts
@@ -1,6 +1,6 @@
 import { Component, DiscordMessage, Embed } from '#lib/types/discord';
 
-const createMessage = {
+const x_messageBuilder = {
   messageObject: {
     username: '',
     avatarUrl: '',
@@ -9,26 +9,42 @@ const createMessage = {
     embeds: [],
     components: [],
   } as DiscordMessage,
-  setUsername(username: string | null = null): void {
+  prepareMessage() {
+    return Object.assign({}, messageBuilder);
+  },
+  setUsername(username: string | null = null) {
     this.messageObject.username = username ?? '';
+
+    return this;
   },
-  setAvatarUrl(avatarUrl: string | null = null): void {
+  setAvatarUrl(avatarUrl: string | null = null) {
     this.messageObject.avatarUrl = avatarUrl ?? '';
+
+    return this;
   },
-  setContent(content: string | null = null): void {
+  setContent(content: string | null = null) {
     this.messageObject.content = content ?? '';
+
+    return this;
   },
-  isTTS(isTTs = false): void {
+  isTTS(isTTs = false) {
     this.messageObject.tts = isTTs;
+
+    return this;
   },
-  appendEmbed(embed: Embed): void {
+  appendEmbed(embed: Embed) {
     this.messageObject.embeds.push(embed);
+
+    return this;
   },
-  appendComponent(component: Component): void {
+  appendComponent(component: Component) {
     this.messageObject.components.push(component);
+
+    return this;
   },
 };
 
+
 export {
-  createMessage,
+  x_messageBuilder,
 };
