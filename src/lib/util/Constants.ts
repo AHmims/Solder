07ee71e2c69e68a1b205e4eebdde19fa3191e77a
Aymane D. Hlamnach <dara-@outlook.jp>
2021-11-16 18:43:59 +0100

    Added util class as a basis for MessageEmbed

---
+++ src/lib/util/Constants.ts
@@ -0,0 +1,11 @@
+// This file's meant to store various constants like colors n error messages etc..
+
+export const Colors: Record<string, number> = {
+  DEFAULT: 0x000000,
+  WHITE: 0xffffff,
+  GREEN: 0x57f287,
+  BLUE: 0x3498db,
+  ORANGE: 0xe67e22,
+  RED: 0xed4245,
+  QUASI_BLACK: 0x23272
+};
